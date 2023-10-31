047224daaedd9614bfac54412323224180080fe9
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-10-31 13:32:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
+
+/ Call the greet and jobGreet methods
+person.greet(); // Output: Hello, my name is Alice, I am 30 years old.
+employee.greet(); // Output: Hello, my name is Bob, I am 25 years old.
+employee.jobGreet(); // Output: Hello, my name
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
